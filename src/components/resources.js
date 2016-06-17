3fae480893be3511924fc022e1564f439064f73f
patw <patw.hi@gmail.com>
2016-06-17 15:55:53 +0800

    Added route and resources component

---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Resources extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Resources</div>
+    );
+  }
+}
