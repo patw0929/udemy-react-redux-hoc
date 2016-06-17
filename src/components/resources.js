c74f739bb8078a720262fb964d5cb54b3e07dcb1
patw <patw.hi@gmail.com>
2016-06-17 15:57:31 +0800

    Revised resources component in functional component

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export default class Resources extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>Resources</div>
-    );
-  }
-}
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Pepper</li>
+        <li>1 Cup Milk</li>
+      </ul>
+    </div>
+  );
+};
