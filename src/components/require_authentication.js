d882496de5e3d9250dd27498df318cba45a5bd68
patw <patw.hi@gmail.com>
2016-06-17 17:17:28 +0800

    Redirect to index if user is not authenticated

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,8 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: PropTypes.object,
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return (
         <ComposedComponent {...this.props} />
